06b5d36a5108f6e4b42df45bf93609cf9f91e1f9
Calebe Oliveira <becalebe@gmail.com>
2013-11-23 23:34:04 -0300

    First version of Paladin. Only to perform a basic query and throw exceptions.

---
+++ DatabaseException.php
@@ -0,0 +1,12 @@
+<?php
+namespace Util;
+
+class DatabaseException extends \Exception {
+	public function __construct($message, $code = 0) {
+		parent::__construct($message, $code);
+	}
+
+	public function __toString() {
+		return "[DatabaseException #{$this->code}]: {$this->message}\n";
+	}
+}
---
+++ Paladin.php
@@ -0,0 +1,46 @@
+<?php
+namespace Util;
+
+require_once 'DatabaseException.php';
+
+class Paladin {
+	protected $pdo;
+
+	public function __construct() {
+		
+		# Database enviroment config.
+		$host   = 'localhost';
+		$dbname = 'database';
+		$user   = 'root';
+		$pass   = '';
+
+
+		try {
+			$this->pdo = new \PDO('mysql:dbname='.$dbname.';host='.$host.';charset=utf8', $user, $pass);
+		} catch(\PDOException $e) {
+			throw new DatabaseException($e->getMessage());
+		}
+	}
+
+	public function query($sql, Array $params = []) {
+		$result = $this->pdo->prepare($sql);
+
+		foreach ($params as $key => $value) {
+			$result->bindParam(":{$key}", ltrim($value, ':'));
+		}
+
+		$result->execute();
+
+		$err = $result->errorInfo();
+		if(!is_null($err[1])) {
+			$this->error($err[2], $err[1]);
+		}
+		
+		return $result->fetchAll(\PDO::FETCH_ASSOC);
+	}
+
+	protected function error($name, $code) {
+		# Send a email? ...
+		throw new DatabaseException($name, $code);
+	}
+}
